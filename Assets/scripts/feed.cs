4d159357498a09ff0cf8c5b6030b2495a1fb9ca1
daniloluca <danilolucasmd@gmail.com>
2016-02-03 23:50:52 -0200

    Adicionado projeto ao repositorio.

---
+++ Assets/scripts/feed.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class feed : MonoBehaviour {
+
+    void OnCollisionEnter2D(Collision2D coll) {
+        DestroyObject(this.gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/scripts/molecule.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class molecule : MonoBehaviour {
+
+    private GameObject[] molecules;
+    private GameObject target;
+    public float speed = 1f;
+    private Vector3 direc;
+    private Vector3 closest;
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        Debug.Log(closest);
+        think();
+    }
+
+    private void think() {
+       // molecules = GameObject.FindGameObjectsWithTag("Feed");
+        for (int i = 0; i < molecules.Length; i++) {
+            Debug.Log(molecules[i]);
+            if (molecules[i] != null) {
+                target = molecules[i];
+                if (Vector3.Distance(transform.position, target.transform.position) <= Vector3.Distance(transform.position, closest)) {
+                    direc = (target.transform.position - this.transform.position).normalized * speed;
+                    closest = direc;
+                }
+            }
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        //molecules = UnityEngine.Object.FindObjectsOfType<GameObject>();
+        molecules = GameObject.FindGameObjectsWithTag("Feed");
+        closest = (molecules[0].transform.position - this.transform.position).normalized * speed;
+
+        think();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Translate(closest * Time.deltaTime);
+    }
+}
